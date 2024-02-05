efb98b805878f65fb0178eac9acb29be68f9dfc6
Dhananji22 <126415251+Dhananji22@users.noreply.github.com>
2024-02-05 21:51:10 +0530

    Frontend setUp

---
+++ Frontend/src/pages/Appoinments.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Components/header";
+import Navbar from "../Components/Navbar";
+
+
+const Appoinments = () => {
+  return (
+   <dev>
+    <Header />
+    <Navbar />
+  </dev> 
+  );
+};
+
+export default Appoinments;
---
+++ Frontend/src/pages/Services.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Components/header";
+import Navbar from "../Components/Navbar";
+
+
+const Services = () => {
+  return (
+   <dev>
+    <Header />
+    <Navbar />
+  </dev> 
+  );
+};
+
+export default Services;
